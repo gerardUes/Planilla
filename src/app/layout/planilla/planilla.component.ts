bf36b143b48157eaa54e3ed19fab538b324c1598
DESKTOP-RU4H6TA\user <davidsaulramirez@hotmail.com>
2021-03-25 16:46:56 -0600

    cambios planilla angular

--- src/app/layout/planilla/planilla.component.ts
+++ src/app/layout/planilla/planilla.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { PlanillaService } from '../servicio/planilla.service';
 
 @Component({
   selector: 'app-planilla',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planilla.component.css']
 })
 export class PlanillaComponent implements OnInit {
+  listadoPlanilla:Array<any>;
+  listaResumen:Array<any>;
+  closeResult: string;
 
-  constructor() { }
+  constructor(private servicio:PlanillaService,private modalService: NgbModal) { 
+
+    let deadline:Date = new Date();
+    console.log(deadline);
+    console.log(deadline.getDate());
+    console.log(deadline.getFullYear());
+    console.log(deadline.getMonth());
+
+
+    this.servicio.obtenerPlanillas(deadline.getMonth(),deadline.getFullYear())
+    .subscribe(dat=>{
+      console.log(JSON.stringify(dat));
+      this.listadoPlanilla=dat;
+    })
+
+  }
 
   ngOnInit(): void {
   }
+
+  verDetalle(data:any){
+  this.servicio.obtenerResumen(data.mes,data.anio,data.tiposPlanilla.tiposPlanillaPK.codTipopla,data.numPlanilla).subscribe(
+    resume=>{
+      this.listaResumen=resume;
+    }
+  );
+  }
+
+  open(content) {
+    this.modalService.open(content,{size: 'lg', windowClass: 'modal-xl'}).result.then(
+        (result) => {
+            this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+    );
+}
+
+private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+    } else {
+        return `with: ${reason}`;
+    }
+}
 
 }
--- src/app/layout/servicio/planilla.service.ts
+++ src/app/layout/servicio/planilla.service.ts
@@ -7,15 +7,27 @@ import { Observable } from 'rxjs';
 })
 export class PlanillaService {
 
+
+  baseUrl:any='http://138.128.245.244:8445/infosweb/api/v1/rrhh/planilla/';
+
+
   constructor(private http:HttpClient) { 
 
 
 
   }
 
-
-
-  obtenerPlanillas():Observable<any>{
-    return this.http.get('http://138.128.245.244:8445');
+obtenerPlanillas(mes:any,anio:any):Observable<any>{
+    return this.http.get('http://138.128.245.244:8445/infosweb/api/v1/rrhh/planilla/programacion-by-anio-mes/3/'+anio+'/'+mes);
   }
+
+
+
+  obtenerResumen(mes:any,anio:any,tipo:any,num:any):Observable<any>{
+    return this.http.get(this.baseUrl+'find-resumen-by-programacion-pla/'+'3/'+anio+'/'+mes+'/'+tipo+'/'+num);
+  }
+
+
+
+
 }
